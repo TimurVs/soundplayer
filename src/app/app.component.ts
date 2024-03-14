a3d2cbd2152e90a65e66d2bc150de62efaed7555
timvoskoboinikov <TVoskoboinikov@sitronics.com>
2024-03-14 20:02:04 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SoundListComponent } from './sound-list/sound-list.component'
-import { PlayerComponent } from './player/player.component'
+// import { PlayerComponent } from './player/player.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SoundListComponent, PlayerComponent ],
+  imports: [RouterOutlet, SoundListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
